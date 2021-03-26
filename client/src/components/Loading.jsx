029b3c56b371ba6f799d725eb3409c8f0e1b42c1
ambergristle <aspanos@protonmail.com>
2021-03-26 15:48:29 -0700

    added loading animation on start

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 
 import { Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+import ReactLoading from "react-loading";
+
+const useStyles = makeStyles((theme) => ({
+  loadingContainer: {
+    height: "50vh",
+    minWidth: "100%",
+  },
+}));
 
 function Loading() {
+  const classes = useStyles();
+
   return (
-    <Grid item xs={12}>
-      <p>Temp text</p>
+    <Grid
+      item
+      xs={12}
+      container
+      justify="center"
+      className={classes.chartContainer}
+    >
+      <ReactLoading
+        type="spin"
+        color="rgba(0,0,0,.25)"
+        height={"20%"}
+        width={"20%"}
+      />
     </Grid>
   );
 }
