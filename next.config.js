eccfed9233d432e927766b6e4616f08544218297
ambergristle <aspanos@protonmail.com>
2021-10-28 08:30:27 -0700

    downgraded next to 11.1.12; restored webpack5 settings to exports

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
-// read .md files with raw-loader
+// // raw-loader complaing that webpack4 || webpack5 dependency missing
+// use webpack5, read .md files with raw-loader
 module.exports = {
+  future: {
+    webpack5: true,
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.md$/,
