e5e31279fc02257753033dff991e55a29c125797
ambergristle <aspanos@protonmail.com>
2021-10-28 08:10:51 -0700

    removed dep webpack5 flag from config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
-// // raw-loader complaing that webpack4 || webpack5 dependency missing
-// use webpack5, read .md files with raw-loader
+// read .md files with raw-loader
 module.exports = {
-  future: {
-    webpack5: true,
-  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.md$/,
