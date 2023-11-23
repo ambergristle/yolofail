4290d7c3f9e9c2af17bfbf7ca868378149a386f9
ambergristle <101149854+ambergristle@users.noreply.github.com>
2023-11-23 09:04:35 -0800

    footer styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import '@/styles/globals.css'
 
+import Link from "next/link";
+
 export const metadata: Metadata = {
   title: 'yolofail',
   description: 'rip your retirement',
@@ -25,8 +27,13 @@ const RootLayout = ({
     <html lang="en">
       <body>
         {children}
-        <footer>
-          hello
+        <footer className="flex flex-row justify-center space-x-4">
+          <Link href="/legal" className="underline-offset-4 hover:underline">
+            {'legal'}
+          </Link>
+          <button className="underline-offset-4 hover:underline">
+            {'feedback'}
+          </button>
         </footer>
       </body>
     </html>
