4bba6e524fc975ce191796990351ce8430473981
ambergristle <101149854+ambergristle@users.noreply.github.com>
2024-02-08 15:40:08 -0800

    build: add railway url key to config getter

--- app/src/config.ts
+++ app/src/config.ts
@@ -1,7 +1,10 @@
 import { EnvError } from '@/lib/errors';
 
 export const getBaseUrl = () => {
-  const baseUrl = process.env.BASE_URL;
+  const railwayUrl = `https://${process.env.RAILWAY_PUBLIC_DOMAIN}`;
+
+  const baseUrl = process.env.BASE_URL ?? railwayUrl;
+  
   if (!baseUrl) throw new EnvError('BASE_URL unset');
   return baseUrl;
 };
